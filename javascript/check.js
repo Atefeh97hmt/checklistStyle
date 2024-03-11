325577229130fd84ed311f9135b255c46ac0e4ac
A.hemmati <A.hemmati@DESKTOP-Q36U9PJ>
2024-03-11 15:46:02 +0330

    c

---
+++ javascript/check.js
@@ -0,0 +1,19 @@
+const clear = document.querySelector(".clear");
+
+const dateelement = document.getElementById("date");
+
+
+const list = document.getElementById("list");
+
+function addToDo(todo){
+const text =     <li class="item">
+<i class="co fa fa-circle-thin" job = "complete"></i>
+<p class="text"> drink coffee </p>
+<i class="de fa fa-trash-o" job="delete"></i>
+</li>
+
+const position="beforeend";
+list.insertAdjacentHTML(position,text);
+}
+const input = document.getElementById("input");
+
